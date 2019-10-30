37fa513a84b5a8cb5533ae560f3d9a4b207806e2
admin <982584989@qq.com>
2019-10-30 15:40:46 +0800

    1

--- src/main/java/com/lujieni/springbootjenkins/controller/JenkinsController.java
+++ src/main/java/com/lujieni/springbootjenkins/controller/JenkinsController.java
@@ -3,6 +3,9 @@ package com.lujieni.springbootjenkins.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Auther ljn
  * @Date 2019/10/29
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsController {
     @RequestMapping("/hello")
     public String hello(){
+        Map<String,String> map = new HashMap<>();
         return "hello Jenkins1";
     }
 }
