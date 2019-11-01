773cda0f144e8dd8881567e9e5eac6cf16b56c24
admin <982584989@qq.com>
2019-11-01 16:44:07 +0800

    1222

--- src/main/java/com/lujieni/springbootjenkins/controller/JenkinsController.java
+++ src/main/java/com/lujieni/springbootjenkins/controller/JenkinsController.java
@@ -15,6 +15,6 @@ public class JenkinsController {
     @RequestMapping("/hello")
     public String hello(){
         Map<String,String> map = new HashMap<>();
-        return "hello Jenkins1";
+        return "hello Jenkins1222";
     }
 }
